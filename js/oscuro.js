7bc1ea206790b60c0921e4307f9a09f23a833216
Cristian <veracristian89@gmail.com>
2022-04-18 12:58:11 -0300

    se cambia event listener por toggle

--- js/oscuro.js
+++ js/oscuro.js
@@ -1,5 +1,4 @@
-let botonOn = document.querySelector(".on");
-let botonOff =document.querySelector(".off");
+let onOffBtn = document.querySelector(".onOff");
 let body = document.querySelector("body");
 let productoInput = document.querySelector(".formulario__producto");
 let precioInput = document.querySelector(".formulario__precio");
@@ -8,29 +7,29 @@ let containerLista = document.querySelector(".container-lista");
 let containerTotal = document.querySelector(".container-total");
 let spanTotal = document.querySelector(".total");
 
-botonOn.addEventListener("click", function(event){
+onOffBtn.addEventListener("click", function(event){
     event.preventDefault();
 
-    body.classList.add("oscuro");  
-    productoInput.classList.add("oscuro-input");
-    precioInput.classList.add("oscuro-input");
-    cantidadInput.classList.add("oscuro-input");
-    containerLista.classList.add("oscuro-input");
-    containerTotal.classList.add("oscuro-input");
-    spanTotal.classList.add("oscuro-input");
+    body.classList.toggle("oscuro");  
+    productoInput.classList.toggle("oscuro-input");
+    precioInput.classList.toggle("oscuro-input");
+    cantidadInput.classList.toggle("oscuro-input");
+    containerLista.classList.toggle("oscuro-input");
+    containerTotal.classList.toggle("oscuro-input");
+    spanTotal.classList.toggle("oscuro-input");
     
 });
 
-botonOff.addEventListener("click", function(event){
-    event.preventDefault();
+// botonOff.addEventListener("click", function(event){
+//     event.preventDefault();
 
-    body.classList.remove("oscuro");
-    producto.classList.remove("oscuro-input") 
-    productoInput.classList.remove("oscuro-input");
-    precioInput.classList.remove("oscuro-input");
-    cantidadInput.classList.remove("oscuro-input");
-    containerLista.classList.remove("oscuro-input");
-    containerTotal.classList.remove("oscuro-input");
-    spanTotal.classList.remove("oscuro-input"); 
+//     body.classList.remove("oscuro");
+//     producto.classList.remove("oscuro-input") 
+//     productoInput.classList.remove("oscuro-input");
+//     precioInput.classList.remove("oscuro-input");
+//     cantidadInput.classList.remove("oscuro-input");
+//     containerLista.classList.remove("oscuro-input");
+//     containerTotal.classList.remove("oscuro-input");
+//     spanTotal.classList.remove("oscuro-input"); 
     
-});
+// });
